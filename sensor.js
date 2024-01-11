a13f1e675fafbb64ec63b62c5876f34cef99684a
arienb <arien.bisschops@student.ucll.be>
2024-01-11 16:29:08 +0100

    bu joh

--- sensor.js
+++ sensor.js
@@ -38,7 +38,6 @@ var temperature = 0;
 var batterij = 0;
 
 function laadData(id){//haalt alle data op,filterd ze, laat ze zien in console en zet ze in variabele, overschrijft de default values van hierboven + id moet megegeven worden
-    console.log(`Loading data for Sensor ID (in laadData sensor.js): ${id}`);
     fetch("http://plantensensor.northeurope.cloudapp.azure.com:11000/api/GetAllDataFromSpecifiedSensor?sensorId="+id)// haalt alles op
     //http://plantensensor.northeurope.cloudapp.azure.com:11000/api/GetAllSensorDataFromAllSensors
     //alle data
